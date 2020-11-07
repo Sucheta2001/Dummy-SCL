5fc44b3c1975cb959c5089b11af175dd12de59d0
sucheta shanbhag <suchetas.cs19@sahyadri.edu.in>
2020-11-07 22:14:26 +0530

    Addition

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,6 @@
+def add(a,b):
+	return a+b
+
 if __name__ == "__main__":
 	while True:
 		print("enter two numbers : ")
@@ -5,6 +8,7 @@ if __name__ == "__main__":
 		print("enter the operation\n1.addition\n2.subtraction\n3.multiplication\n4.division\n5.squareroot\n6.power(a^b)\n7.remainder(a mod b)\n8.exit")
 		operator = input()
 		if(operator == '1'):
+			print(add(operand1,operand2))
 		elif(operator == '2'):
 		elif(operator == '3'):
 		elif(operator == '4'):
